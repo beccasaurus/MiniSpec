e1d82ac64d0fb42712d7d62d27a212c436a3e88b
Rebecca Taylor <bex@beccasaur.us>
2021-04-28 16:53:11 -0700

    dotnet tool setup

---
+++ Client.Console/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Client
+{
+    public class Class1
+    {
+       public bool TestAThing() {
+           return true;
+       }
+
+       public bool TestAnotherThing() {
+           return false;
+       }
+    }
+}
